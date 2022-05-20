88a69a0b2afff915c54007fe446e061a8a8ac45a
JosipTr <josiptroha@gmail.com>
2022-05-20 12:12:53 +0200

    Create ShoppingListItem class

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Shopping List"),
         ),
+        body: ShoppingListItem(product: Product(name: "Hammer"), inCart: true, onCartChanged: (product, inCart) {}),
       ),
     );
   }
@@ -27,3 +28,28 @@ class Product {
 
 typedef CartChangedCallback = Function(Product product, bool inCart);
 
+class ShoppingListItem extends StatelessWidget {
+  final Product product;
+  final bool inCart;
+  final CartChangedCallback onCartChanged;
+
+  ShoppingListItem(
+      {Key? key,
+      required this.product,
+      required this.inCart,
+      required this.onCartChanged})
+      : super(key: ObjectKey(product));
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ListTile(
+        leading: CircleAvatar(
+          backgroundColor: Colors.red,
+          child: Text(product.name[0]), 
+        ),
+        title: Text(product.name)
+      ),
+    );
+  }
+}
