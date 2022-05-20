4b4343ffa1616e8b998f64784aca401efc395c19
JosipTr <josiptroha@gmail.com>
2022-05-20 13:25:05 +0200

    Finish app

--- lib/main.dart
+++ lib/main.dart
@@ -10,20 +10,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Shopping List"),
-        ),
-        body: Center(
-          child: ShoppingListItem(product: Product(name: "Hammer"), inCart: true, onCartChanged: (product, inCart) {}),
-        ),
+      home: ShoppingList(
+        products: [
+          Product(name: "Cheese"),
+          Product(name: "Bread"),
+          Product(name: "Garlic"),
+          Product(name: "Onion")
+        ],
       ),
     );
   }
 }
-
-
-
 
 class Product {
   final String name;
@@ -31,9 +28,7 @@ class Product {
   Product({required this.name});
 }
 
-
 typedef CartChangedCallback = Function(Product product, bool inCart);
-
 
 class ShoppingListItem extends StatelessWidget {
   final Product product;
@@ -48,9 +43,7 @@ class ShoppingListItem extends StatelessWidget {
       : super(key: ObjectKey(product));
 
   Color _getColor() {
-    return inCart
-        ? Colors.green
-        : Colors.red;
+    return inCart ? Colors.green : Colors.red;
   }
 
   Icon _getIcon() {
@@ -60,22 +53,65 @@ class ShoppingListItem extends StatelessWidget {
   }
 
   TextStyle? _getTextStyle() {
-    if (!inCart) return const TextStyle(color: Colors.red, fontWeight: FontWeight.bold);
-    return const TextStyle(fontStyle: FontStyle.italic, decoration: TextDecoration.lineThrough, color: Colors.green);
+    if (!inCart) {
+      return const TextStyle(color: Colors.red, fontWeight: FontWeight.bold);
+    }
+    return const TextStyle(
+        fontStyle: FontStyle.italic,
+        decoration: TextDecoration.lineThrough,
+        color: Colors.green);
   }
-
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: () {
-        onCartChanged(product, inCart);
-      },
-      leading: CircleAvatar(
-        backgroundColor: _getColor(),
-        child: _getIcon(), 
+        onTap: () {
+          onCartChanged(product, inCart);
+        },
+        leading: CircleAvatar(
+          backgroundColor: _getColor(),
+          child: _getIcon(),
+        ),
+        title: Text(product.name, style: _getTextStyle()));
+  }
+}
+
+class ShoppingList extends StatefulWidget {
+  final List<Product> products;
+  const ShoppingList({Key? key, required this.products}) : super(key: key);
+
+  @override
+  State<ShoppingList> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (inCart) {
+        _shoppingCart.remove(product);
+      } else {
+        _shoppingCart.add(product);
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Shopping List"),
       ),
-      title: Text(product.name, style: _getTextStyle())
+      body: ListView(
+        children: widget.products.map((Product product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      ),
     );
   }
 }
