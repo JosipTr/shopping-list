e0ab471a68945c484065d9001bbaeccb3478a5ca
JosipTr <josiptroha@gmail.com>
2022-05-20 12:18:07 +0200

    Add _getColor function in ShoppingListItem class

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,18 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+
 class Product {
   final String name;
 
   Product({required this.name});
 }
 
+
 typedef CartChangedCallback = Function(Product product, bool inCart);
+
 
 class ShoppingListItem extends StatelessWidget {
   final Product product;
@@ -42,9 +47,18 @@ class ShoppingListItem extends StatelessWidget {
       required this.onCartChanged})
       : super(key: ObjectKey(product));
 
+  Color _getColor(BuildContext context) {
+    return inCart
+        ? Colors.black54
+        : Theme.of(context).primaryColor;
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap: () {
+        onCartChanged(product, inCart);
+      },
       leading: CircleAvatar(
         backgroundColor: Colors.red,
         child: Text(product.name[0]), 
